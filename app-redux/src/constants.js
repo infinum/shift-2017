c758e78ec35a97a627d40d0bce8fef2e38466c1f
Darko Kukovec <darko@kukovec.eu>
2017-05-29 15:45:44 +0200

    [Redux] Add action names to constants

---
+++ app-redux/src/constants.js
@@ -0,0 +1,7 @@
+export const DATA_LOAD = 'DATA_LOAD';
+export const DATA_LOAD_DONE = 'DATA_LOAD_DONE';
+export const DATA_LOAD_ERROR = 'DATA_LOAD_ERROR';
+
+export const TALK_FAV_TOGGLE = 'TALK_FAV_TOGGLE';
+export const TALK_SELECT = 'TALK_SELECT';
+export const FILTER_CHANGE = 'FILTER_CHANGE';
