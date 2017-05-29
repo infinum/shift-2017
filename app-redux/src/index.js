ff0cd85168b306388af07990baaa955314868ea9
Darko Kukovec <darko@kukovec.eu>
2017-05-29 15:50:19 +0200

    [Redux] Initialize the store

--- app-redux/src/index.js
+++ app-redux/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducer';
+import {loadData} from './actions';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+store.dispatch(loadData());
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
